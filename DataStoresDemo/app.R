2925044936a6cd9cae5a9214108874da4234f348
Niket Doshi <niket.doshi@thinkbiganalytics.com>
2017-04-07 01:54:21 +0200

    Added spark context

--- DataStoresDemo/app.R
+++ DataStoresDemo/app.R
@@ -1,30 +1,66 @@
-renderLeafletsource("global.R")
+source("global.R")
+library(shiny)
 
-ui <- fluidPage(
-  br(),
-  br(),
-  textAreaInput("q1", "Enter the SQL Query:", 'SELECT * FROM City LIMIT 100',width = '100%', '30%' ),
-  actionButton("runquery", "Run"),
-  #uiOutput("panels")
-  br(),
-  br(),
-  
-  tabsetPanel(
-    tabPanel("Summary",dataTableOutput("tbl")),
-    tabPanel("Plot", PlotBuildR::generatePlotUI("plot1"))
+ui <- navbarPage(
+  title = "PlotBuildR",
+  tabPanel(
+    "MySQL Context",
+    fluidPage(
+      fluidRow(
+        textAreaInput("mysql", "Enter the SQL Query:", 'SELECT * FROM City LIMIT 100',width = '100%', '30%'),
+        actionButton("sqlQueryButton", "Run")
+      ),
+      br(),
+      fluidRow(
+        tabsetPanel(
+          tabPanel("Summary",
+                   br(),
+                   dataTableOutput("tbl")
+          ),
+          tabPanel("Plot",
+                   PlotBuildR::plotBuildROutput("plot1")
+          )
+        )
+      )
+    )
+  ),
+  tabPanel(
+    "Spark Context",
+    fluidPage(
+      fluidRow(
+        radioButtons("sparkChoice", label = "Select Spark Context", choices = c("Hive", "dplyr"), inline = T)
+      ),
+      fluidRow(
+        textAreaInput("spark", value = NULL, width = '100%', '30%'),
+        actionButton("sparkQueryButton", "Run")
+      ),
+      br(),
+      fluidRow(
+        tabsetPanel(
+          tabPanel("Summary",
+                   br(),
+                   #verbatimTextOutput("print1")
+                   dataTableOutput("tbl_spark")
+          ),
+          tabPanel("Plot",
+                   PlotBuildR::plotBuildROutput("plot_spark")
+          )
+        )
+      )
+    )
   )
+  
 )
 
 server <- function(input, output, session) {
   
-  query1 <- reactiveValues(data = NULL)
+  ## MYSQL ####
   
-  query1$data <- reactive({
-    input$runquery
-    selected_query <- isolate(input$q1)
+  mysql_query <- eventReactive(input$sqlQueryButton, {
+    selected_query <- isolate(input$mysql)
   })
   
-  data <- reactive({
+  mysql_data <- reactive({
     conn <- dbConnect(
       drv = RMySQL::MySQL(),
       dbname = "shinydemo",
@@ -32,24 +68,62 @@ server <- function(input, output, session) {
       username = "guest",
       password = "guest")
     on.exit(dbDisconnect(conn), add = TRUE)
-    dbGetQuery(conn, paste0(query1$data()))
+    dbGetQuery(conn, paste0(mysql_query()))
   })
   
-  reactive_list <- list()
-  reactive_list$df <- reactive({data()})
+  reactive_list_mysql <- list()
+  reactive_list_mysql$df <- reactive({mysql_data()})
   
-  output$tbl <- renderDataTable({data()})
-  callModule(generatePlot, "plot1", df_list = df_list, reactive_df = reactive_list, 
-             df = "df", title = "Demo SQL Plot")
+  output$tbl <- renderDataTable({mysql_data()})
+  PlotBuildR::renderPlotBuildR("plot1", reactive_df = reactive_list_mysql, 
+                               df = "df", plot_type = "Bar Plot", title = "Demo SQL Plot")
   
-  # observeEvent(input$runquery,{
-  #   output$panels=renderUI({
-  #   tabsetPanel(
-  #     tabPanel("Summary",dataTableOutput("tbl")),
-  #     tabPanel("Plot", PlotBuildR::generatePlotUI("plot1"))
-  #   )
-  #   })
-  # })
+  
+  ## Spark ####
+  
+  sparkChoiceSelected <- reactive({
+    input$sparkChoice
+  })
+  
+  observe({
+    validate(need(input$sparkChoice, F))
+    
+    switch(sparkChoiceSelected(),
+           "Hive" = updateTextAreaInput(session, "spark", label = "Enter the Hive Query:", value = 'SELECT * FROM quakes_geo LIMIT 10'),
+           "dplyr" = updateTextAreaInput(session, "spark", label = "Enter the dplyr operation", value = 'quakes_geo_sdf %>% head(10)')
+    )
+  })
+  
+  spark_query <- eventReactive(input$sparkQueryButton, {
+    selected_query <- isolate(input$spark)
+  })
+  
+  output$print1 <- renderText({
+    spark_query()
+  })
+  
+  spark_data <- reactive({
+    switch(sparkChoiceSelected(),
+           "Hive" = {
+             dbGetQuery(sc, spark_query())
+           },
+           "dplyr" = {
+             eval(parse(text = spark_query()))
+           }
+    )
+  })
+  
+  reactive_list_spark <- list()
+  reactive_list_spark$df <- reactive({
+    spark_data() %>% collect
+  })
+  
+  output$tbl_spark <- renderDataTable({
+    spark_data()
+  })
+  
+  PlotBuildR::renderPlotBuildR("plot_spark", reactive_df = reactive_list_spark, df = df, plot_type = "Map")
+  
 }
 
 shinyApp(ui, server)
--- DataStoresDemo/global.R
+++ DataStoresDemo/global.R
@@ -1,3 +1,4 @@
+if(!require(devtools)) {install.packages("devtools", dependencies = TRUE);require(devtools)}
 if(!require(shiny)) {devtools::install_github("rstudio/shiny");require(shiny)}
 if(!require(shinydashboard)) {install.packages("shinydashboard", dependencies = TRUE);require(shinydashboard)}
 if(!require(shinyjs)) {install.packages("shinyjs", dependencies = TRUE);require(shinyjs)}
@@ -6,18 +7,25 @@ if(!require(shinythemes)) {install.packages("shinythemes", dependencies = TRUE);
 if(!require(plotly)) {install.packages("plotly");require(plotly)}
 if(!require(plyr)) {install.packages("plyr", dependencies = TRUE);require(plyr)}
 if(!require(dplyr)) {install.packages("dplyr");require(dplyr)}
-if(!require(stringr)) {install.packages("stringr", dependencies = TRUE);require(stringr)}
-#if(!require(flexdashboard)) {install.packages("flexdashboard", dependencies = TRUE);require(flexdashboard)}
-if(!require(networkD3)) {install.packages("networkD3", dependencies = TRUE);require(networkD3)}
 if(!require(tidyr)) {install.packages("tidyr", dependencies = TRUE);require(tidyr)}
+if(!require(stringr)) {install.packages("stringr", dependencies = TRUE);require(stringr)}
+if(!require(networkD3)) {install.packages("networkD3", dependencies = TRUE);require(networkD3)}
+if(!require(leaflet)) {install.packages("leaflet", dependencies = TRUE);require(leaflet)}
+if(!require(parcoords)) {devtools::install_github("timelyportfolio/parcoords");require(parcoords)}
 if(!require(fasttime)) {install.packages("fasttime", dependencies = TRUE);require(fasttime)}
 if(!require(lubridate)) {install.packages("lubridate", dependencies = TRUE);require(lubridate)}
 options(lubridate.fasttime = TRUE)
-library(devtools)
-if(!require(PlotBuildR)){install_github('ThinkBigAnalytics/PlotBuildR', auth_token='77fdd51877a897509be91b73e1c351b1b4df1301'); require(PlotBuildR)}
-library(PlotBuildR)
+if(!require(PlotBuildR)){install_github('ThinkBigAnalytics/PlotBuildR', auth_token='acf4f5f2e1fe3cb2e07ef81991bc4fd4773dc5d2'); require(PlotBuildR)}
 library(DBI)
 if(!require(RMySQL)) {install.packages('RMySQL', dependencies = TRUE); require(PlotBuildR)}
+if(!require(sparklyr)) {install.packages('sparklyr', dependencies = TRUE); require(sparklyr)}
+if(!require(xml2)) {install.packages('xml2', dependencies = TRUE); require(xml2)}
 
-data1 <- iris 
-df_list = ls() 
+Sys.setenv(SPARK_HOME='/usr/hdp/current/spark-client')
+Sys.setenv(SPARK_MASTER_IP = "data-lab.io")
+sc <- spark_connect(master = "yarn-client", config = spark_config())
+
+dbGetQuery(sc, "USE quakesdb")
+dbGetQuery(sc, "LOAD DATA INPATH '/user/nd186031/quakes.csv' INTO TABLE quakes_geo") #path: /user/nd186031/quakes.csv
+
+quakes_geo_sdf <- tbl(sc, "quakes_geo") %>% sdf_register()
